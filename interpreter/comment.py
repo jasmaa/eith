f849849e0203785fd4591e1f56025b9aa91bcd03
jasmaa <jasonmaa3955@gmail.com>
2021-08-22 13:19:38 -0400

    Add NotCommentException back to uncomment

--- interpreter/comment.py
+++ interpreter/comment.py
@@ -8,7 +8,7 @@ class CommentInterpreterMixin:
     def interp_is_comment(self):
         idx = self.stack.pop() - 1
         self.stack.append(-1 if self.raw_lines[idx][:2] == '\ ' else 0)
-    
+
     def interp_is_comment_range(self):
         b = self.stack.pop() - 1
         a = self.stack.pop() - 1
@@ -33,6 +33,8 @@ class CommentInterpreterMixin:
         idx = self.stack.pop() - 1
         if self.raw_lines[idx][:2] == '\ ':
             self.raw_lines[idx] = self.raw_lines[idx][2:]
+        else:
+            raise NotCommentException
 
     def interp_toggle_comment(self):
         idx = self.stack.pop() - 1
@@ -53,6 +55,8 @@ class CommentInterpreterMixin:
         for i in range(a, b):
             if self.raw_lines[i][:2] == '\ ':
                 self.raw_lines[i] = self.raw_lines[i][2:]
+            else:
+                raise NotCommentException
 
     def toggle_comment_range(self):
         b = self.stack.pop() - 1
